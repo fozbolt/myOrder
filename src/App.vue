107a9f5351d9b15d97b6d196341d8a419cb37568
FiliP <filip.ozbolt@windowslive.com>
2022-06-02 20:45:59 +0200

    navbar and home basics

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,48 @@
 
 <template>
   <div id="app">
-    <nav v-if="!isAuthenticated">
+    <!--
+      <nav v-if="isAuthenticated">
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link>
+    </nav>
+    -->
+   <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+          </ul>
+        </div>
+        <a class="navbar-brand ms-auto" href="#">
+          <img class="logo center" alt="logo center" src="./assets/logo-main.svg" />
+        </a>
+      </div>
+      
     </nav>
   
 
     <router-view/>
   </div>
 </template>
+
 
 <script>
 import store from '@/store.js'
@@ -54,4 +87,14 @@ nav {
     }
   }
 }
+
+.navbar-collapse {
+    position: absolute;
+    width: 95%;
+    top: 100%;
+    background-color: gray;
+}
+
+
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="home">
-    
+  <div class="container">
+    <div class="content" id="landingContent"></div>
+    <div class="content" id="aboutContent"></div>
+    <div class="content"></div>
+    <div class="content"></div>
+    <div class="content"></div>
   </div>
 </template>
 
@@ -15,3 +19,20 @@ export default {
   }
 }
 </script>
+
+<style>
+
+
+.content {
+    height: 100vh;
+}
+
+#landingContent{
+  background-color: blue;
+}
+
+#aboutContent{
+  background-color: pink;
+}
+
+</style>
